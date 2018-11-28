08bdae218488514b091c412128276d79e3580c8b
nhiyobie <nytran92@gmail.com>
2018-11-28 08:27:48 -0700

    Update preprocessing.py

--- project-code/src/data/preprocessing.py
+++ project-code/src/data/preprocessing.py
@@ -1,46 +1,44 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Tue Nov 27 20:26:39 2018
-
-@author: NhiYobie
-"""
-
 
 import pandas as pd
 import click
 
 def read_raw_data(input_file):
-    df = pd.read_csv(input_file, header=None,sep=',')
+    df = pd.read_csv(input_file, sep=',')
     return df
 
+def read_pickle(input_file):
+    df = pd.read_pickle(input_file)
+    return df
 
-
-def preprocessing(training):
+def preprocessing(df):
     
     #Replace missing or bad data with median value
-    training['NumberOfDependents']= training['NumberOfDependents'].fillna((training['NumberOfDependents'].median()))
-    training['MonthlyIncome']= training['MonthlyIncome'].fillna(training['MonthlyIncome'].median())
-    training_prep=training.replace({'age': {0: training['age'].median()}}) 
+    df['NumberOfDependents']= df['NumberOfDependents'].fillna((df['NumberOfDependents'].median()))
+    df['MonthlyIncome']= df['MonthlyIncome'].fillna(df['MonthlyIncome'].median())
+    df_prep=df.replace({'age': {0: training['age'].median()}}) 
     
     #drop DebtRatio due to low correllation 
-    training_prep=training_prep.drop('DebtRatio', 1)
+    df_prep=df_prep.drop('DebtRatio', 1)
     
     #feature engineering by adding highly correlated features into one new feature
 
-    training_prep['TotalNumberofTimesPastDue']= training_prep['NumberOfTime30-59DaysPastDueNotWorse'] + training_prep['NumberOfTime60-89DaysPastDueNotWorse'] + training_prep['NumberOfTimes90DaysLate'] 
-    training_prep=training_prep.drop('NumberOfTime30-59DaysPastDueNotWorse', 1)
-    training_prep=training_prep.drop('NumberOfTime60-89DaysPastDueNotWorse', 1)
-    training_prep=training_prep.drop('NumberOfTimes90DaysLate', 1)
+    df_prep['TotalNumberofTimesPastDue']= df_prep['NumberOfTime30-59DaysPastDueNotWorse'] + df_prep['NumberOfTime60-89DaysPastDueNotWorse'] + df_prep['NumberOfTimes90DaysLate'] 
+    df_prep=df_prep.drop('NumberOfTime30-59DaysPastDueNotWorse', 1)
+    df_prep=df_prep.drop('NumberOfTime60-89DaysPastDueNotWorse', 1)
+    df_prep=df_prep.drop('NumberOfTimes90DaysLate', 1)
     
     #drop highly correlated features 
-    training_prep['TotalNumberOfOpenLines']= training_prep['NumberOfOpenCreditLinesAndLoans'] + training_prep['NumberRealEstateLoansOrLines'] 
-    training_prep=training_prep.drop('NumberOfOpenCreditLinesAndLoans', 1)
-    training_prep=training_prep.drop('NumberRealEstateLoansOrLines', 1)
+    df_prep['TotalNumberOfOpenLines']= training_prep['NumberOfOpenCreditLinesAndLoans'] + training_prep['NumberRealEstateLoansOrLines'] 
+    df_prep=df_prep.drop('NumberOfOpenCreditLinesAndLoans', 1)
+    df_prep=df_prep.drop('NumberRealEstateLoansOrLines', 1)
 
     #drop TotalNumberOfOpenLines due to low correllation 
-    training_prep=training_prep.drop('TotalNumberOfOpenLines', 1)
-    
-    return training_prep
+    df_prep=df_prep.drop('TotalNumberOfOpenLines', 1)
+
+
+
+    return df_prep
 
 @click.command()
 @click.argument('input_file', type=click.Path(exists=True, readable=True, dir_okay=False))
