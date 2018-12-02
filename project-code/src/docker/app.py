6bd31728191004b9ad1dbe33632f95a368bd4972
nhiyobie <nytran92@gmail.com>
2018-12-02 00:01:42 -0700

    Update and rename test_app.py to app.py

--- project-code/src/docker/app.py
+++ project-code/src/docker/app.py
@@ -33,23 +33,35 @@ def json_get_results():
 
     x=df[['RevolvingUtilizationOfUnsecuredLines','age','MonthlyIncome','NumberOfDependents','TotalNumberofTimesPastDue']]
 
-    print(x)
-    
-    df_result=df['Unnamed: 0']
+    df_result=pd.DataFrame()
+    df_result['ID']=df['Unnamed: 0']
 
-    with open('xgboost_model.pkl','rb') as f:
+    with open('src/docker/xgboost_model.pkl','rb') as f:
     	model = pickle.load(f)
     
     y_predict=model.predict(x)
 
-    df_result['SeriousDlqin2yrs']=y_predict
-    print(df_result)
+    y_predict_df=pd.DataFrame(y_predict)
+
+    df_result['SeriousDlqin2yrs']=y_predict_df
+
+    data= df_result.to_json(orient='records')
+    with open('data/processed/results.json', 'w') as fp:
+        	fp.write(data + '\n')
 
     return 'JSON returned'
 
 if __name__ == '__main__':
 
     app.run(host="0.0.0.0", port=80)
+
+
+
+
+
+
+
+
 
 
 
